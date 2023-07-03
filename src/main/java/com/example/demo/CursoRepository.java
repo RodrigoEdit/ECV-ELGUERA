54e1c429f1dcf518b4af5eb1a32d49141f4afe4d
RodrigoEdit <elgueraamadorodrigo@gmail.com>
2023-07-03 12:19:28 -0500

    update

--- src/main/java/com/example/demo/CursoRepository.java
+++ src/main/java/com/example/demo/CursoRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo;
+
 import org.springframework.data.repository.CrudRepository;
 
-public class CursoRepository extends CrudRepository<Curso, Integer> {
+public interface CursoRepository extends CrudRepository<Curso,Integer> {
     
 }
